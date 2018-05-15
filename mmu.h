3c87e8b6f6fab1cb7adbdc7d1a4262b0b4f4b9f1
Keiji Suzuki <zuki.ebetsu@gmail.com>
2018-05-15 17:28:50 +0900

    translate the comments in mmu.h

--- mmu.h
+++ mmu.h
@@ -1,73 +1,73 @@
-// This file contains definitions for the
-// x86 memory management unit (MMU).
+// このファイルはx86メモリ管理ユニット(MMU)
+// のための定義を含んでいる。
 
-// Eflags register
-#define FL_CF           0x00000001      // Carry Flag
-#define FL_PF           0x00000004      // Parity Flag
-#define FL_AF           0x00000010      // Auxiliary carry Flag
-#define FL_ZF           0x00000040      // Zero Flag
-#define FL_SF           0x00000080      // Sign Flag
-#define FL_TF           0x00000100      // Trap Flag
-#define FL_IF           0x00000200      // Interrupt Enable
-#define FL_DF           0x00000400      // Direction Flag
-#define FL_OF           0x00000800      // Overflow Flag
-#define FL_IOPL_MASK    0x00003000      // I/O Privilege Level bitmask
+// Eflagsレジスタ
+#define FL_CF           0x00000001      //  1: キャリーフラグ
+#define FL_PF           0x00000004      //  2: パリティフラグ
+#define FL_AF           0x00000010      //  4: 補助キャリーフラグ
+#define FL_ZF           0x00000040      //  6: ゼロフラグ
+#define FL_SF           0x00000080      //  7: サインフラグ
+#define FL_TF           0x00000100      //  8: トラップフラグ
+#define FL_IF           0x00000200      //  9: 割り込みを有効化
+#define FL_DF           0x00000400      // 10: ディレクションフラグ
+#define FL_OF           0x00000800      // 11: オーバーフローフラグ
+#define FL_IOPL_MASK    0x00003000      // 12-13: I/O特権レベルビットマスク
 #define FL_IOPL_0       0x00000000      //   IOPL == 0
 #define FL_IOPL_1       0x00001000      //   IOPL == 1
 #define FL_IOPL_2       0x00002000      //   IOPL == 2
 #define FL_IOPL_3       0x00003000      //   IOPL == 3
-#define FL_NT           0x00004000      // Nested Task
-#define FL_RF           0x00010000      // Resume Flag
-#define FL_VM           0x00020000      // Virtual 8086 mode
-#define FL_AC           0x00040000      // Alignment Check
-#define FL_VIF          0x00080000      // Virtual Interrupt Flag
-#define FL_VIP          0x00100000      // Virtual Interrupt Pending
-#define FL_ID           0x00200000      // ID flag
+#define FL_NT           0x00004000      // 14: ネストタスク
+#define FL_RF           0x00010000      // 16: レジュームフラグ
+#define FL_VM           0x00020000      // 17: 仮想8086モード
+#define FL_AC           0x00040000      // 18: アライメントチェック
+#define FL_VIF          0x00080000      // 19: 仮想割り込みフラグ
+#define FL_VIP          0x00100000      // 20: 仮想割り込みペンディング
+#define FL_ID           0x00200000      // 21: IDフラグ
 
-// Control Register flags
-#define CR0_PE          0x00000001      // Protection Enable
-#define CR0_MP          0x00000002      // Monitor coProcessor
-#define CR0_EM          0x00000004      // Emulation
-#define CR0_TS          0x00000008      // Task Switched
-#define CR0_ET          0x00000010      // Extension Type
-#define CR0_NE          0x00000020      // Numeric Errror
-#define CR0_WP          0x00010000      // Write Protect
-#define CR0_AM          0x00040000      // Alignment Mask
-#define CR0_NW          0x20000000      // Not Writethrough
-#define CR0_CD          0x40000000      // Cache Disable
-#define CR0_PG          0x80000000      // Paging
+// コントロールレジスタフラグ
+#define CR0_PE          0x00000001      //  0: プロテクトモードを有効化
+#define CR0_MP          0x00000002      //  1: モニタコプロセッサ
+#define CR0_EM          0x00000004      //  2: エミュレーション
+#define CR0_TS          0x00000008      //  3: タスクスイッチ
+#define CR0_ET          0x00000010      //  4: 拡張タイプ
+#define CR0_NE          0x00000020      //  5: 数値演算エラー
+#define CR0_WP          0x00010000      // 16: 書き込み保護
+#define CR0_AM          0x00040000      // 18: アライメントマスク
+#define CR0_NW          0x20000000      // 29: ノットライトスルー
+#define CR0_CD          0x40000000      // 30: キャッシュ無効化
+#define CR0_PG          0x80000000      // 31: ページング
 
-#define CR4_PSE         0x00000010      // Page size extension
+#define CR4_PSE         0x00000010      // 4: ページサイズ拡張
 
-// various segment selectors.
-#define SEG_KCODE 1  // kernel code
-#define SEG_KDATA 2  // kernel data+stack
-#define SEG_UCODE 3  // user code
-#define SEG_UDATA 4  // user data+stack
-#define SEG_TSS   5  // this process's task state
+// セグメントセレクタの定義
+#define SEG_KCODE 1  // カーネルコード
+#define SEG_KDATA 2  // カーネルデータとスタック
+#define SEG_UCODE 3  // ユーザコード
+#define SEG_UDATA 4  // ユーザデータとスタック
+#define SEG_TSS   5  // このプロセスのタスクステート
 
-// cpu->gdt[NSEGS] holds the above segments.
+// cpu->gdt[NSEGS] は上記のセグメントを保持する
 #define NSEGS     6
 
 #ifndef __ASSEMBLER__
-// Segment Descriptor
+// セグメントディスクリプタ
 struct segdesc {
-  uint lim_15_0 : 16;  // Low bits of segment limit
-  uint base_15_0 : 16; // Low bits of segment base address
-  uint base_23_16 : 8; // Middle bits of segment base address
-  uint type : 4;       // Segment type (see STS_ constants)
-  uint s : 1;          // 0 = system, 1 = application
-  uint dpl : 2;        // Descriptor Privilege Level
-  uint p : 1;          // Present
-  uint lim_19_16 : 4;  // High bits of segment limit
-  uint avl : 1;        // Unused (available for software use)
-  uint rsv1 : 1;       // Reserved
-  uint db : 1;         // 0 = 16-bit segment, 1 = 32-bit segment
-  uint g : 1;          // Granularity: limit scaled by 4K when set
-  uint base_31_24 : 8; // High bits of segment base address
+  uint lim_15_0 : 16;  // セグメントリミット値の低位ビット
+  uint base_15_0 : 16; // セグメントベースアドレスの低位ビット
+  uint base_23_16 : 8; // セグメントベースアドレスの中位ビット
+  uint type : 4;       // セグメントタイプ（STS_ 定数を参照）
+  uint s : 1;          // 0 = システム, 1 = ・アプリケーション
+  uint dpl : 2;        // ディスクリプタの特権レベル
+  uint p : 1;          // 存在する
+  uint lim_19_16 : 4;  // セグメントリミット値の高位ビット
+  uint avl : 1;        // 未使用（ソフトウェアで利用可能）
+  uint rsv1 : 1;       // 予約済
+  uint db : 1;         // 0 = 16 ビットセグメント, 1 = 32 ビットセグメント
+  uint g : 1;          // 単位: セットされるとリミット値が4K倍される
+  uint base_31_24 : 8; // セグメントベースアドレスの高位ビット
 };
 
-// Normal segment
+// 普通のセグメント
 #define SEG(type, base, lim, dpl) (struct segdesc)    \
 { ((lim) >> 12) & 0xffff, (uint)(base) & 0xffff,      \
   ((uint)(base) >> 16) & 0xff, type, 1, dpl, 1,       \
@@ -78,82 +78,82 @@ struct segdesc {
   (uint)(lim) >> 16, 0, 0, 1, 0, (uint)(base) >> 24 }
 #endif
 
-#define DPL_USER    0x3     // User DPL
+#define DPL_USER    0x3     // ユーザDPL
 
-// Application segment type bits
-#define STA_X       0x8     // Executable segment
-#define STA_E       0x4     // Expand down (non-executable segments)
-#define STA_C       0x4     // Conforming code segment (executable only)
-#define STA_W       0x2     // Writeable (non-executable segments)
-#define STA_R       0x2     // Readable (executable segments)
-#define STA_A       0x1     // Accessed
+// アプリケーションセグメントのタイプビット
+#define STA_X       0x8     // 実行可能セグメント
+#define STA_E       0x4     // 拡大縮小（非実行可能セグメント）
+#define STA_C       0x4     // コンフォーミングコードセグメント（実行可能のみ）
+#define STA_W       0x2     // 書き込み可能（非実行可能セグメント）
+#define STA_R       0x2     // 読み取り可能（実行可能セグメント）
+#define STA_A       0x1     // アクセス
 
-// System segment type bits
-#define STS_T16A    0x1     // Available 16-bit TSS
-#define STS_LDT     0x2     // Local Descriptor Table
-#define STS_T16B    0x3     // Busy 16-bit TSS
-#define STS_CG16    0x4     // 16-bit Call Gate
-#define STS_TG      0x5     // Task Gate / Coum Transmitions
-#define STS_IG16    0x6     // 16-bit Interrupt Gate
-#define STS_TG16    0x7     // 16-bit Trap Gate
-#define STS_T32A    0x9     // Available 32-bit TSS
-#define STS_T32B    0xB     // Busy 32-bit TSS
-#define STS_CG32    0xC     // 32-bit Call Gate
-#define STS_IG32    0xE     // 32-bit Interrupt Gate
-#define STS_TG32    0xF     // 32-bit Trap Gate
+// システムセグメントのタイプビット
+#define STS_T16A    0x1     // 16ビットTSSを利用可能
+#define STS_LDT     0x2     // ローカルディスクリプタテーブル
+#define STS_T16B    0x3     // ビジーな16ビットTSS
+#define STS_CG16    0x4     // 16ビットコールゲート
+#define STS_TG      0x5     // タスクゲート / Coum Transmitions
+#define STS_IG16    0x6     // 16ビット割り込みゲート
+#define STS_TG16    0x7     // 16ビットトラップゲート
+#define STS_T32A    0x9     // 32ビットTSSを利用可能
+#define STS_T32B    0xB     // ビジーな32ビットTSS
+#define STS_CG32    0xC     // 32ビットコールゲート
+#define STS_IG32    0xE     // 32ビット割り込みゲート
+#define STS_TG32    0xF     // 32ビットトラップゲート
 
-// A virtual address 'la' has a three-part structure as follows:
+// 仮想アドレス 'la' は次の3つの部分からなる構造をもつ:
 //
-// +--------10------+-------10-------+---------12----------+
-// | Page Directory |   Page Table   | Offset within Page  |
-// |      Index     |      Index     |                     |
-// +----------------+----------------+---------------------+
-//  \--- PDX(va) --/ \--- PTX(va) --/
+// +--------10---------+-------10---------+---------12----------+
+// | ページディレクトリ| ページテーブル   | ページ内オフセット  |
+// |   インデックス    | インデックス     |                     |
+// +-------------------+------------------+---------------------+
+// \---- PDX(va) ------/\--- PTX(va) -----/
 
-// page directory index
+// ページディレクトリインデックス
 #define PDX(va)         (((uint)(va) >> PDXSHIFT) & 0x3FF)
 
-// page table index
+// ページテーブルインデックス
 #define PTX(va)         (((uint)(va) >> PTXSHIFT) & 0x3FF)
 
-// construct virtual address from indexes and offset
+// インデックスとオフセットから仮想アドレスを構築
 #define PGADDR(d, t, o) ((uint)((d) << PDXSHIFT | (t) << PTXSHIFT | (o)))
 
-// Page directory and page table constants.
-#define NPDENTRIES      1024    // # directory entries per page directory
-#define NPTENTRIES      1024    // # PTEs per page table
-#define PGSIZE          4096    // bytes mapped by a page
+// ページディレクトリとページテーブルの定数
+#define NPDENTRIES      1024    // PGDIRあたりのディレクトリエントリの数
+#define NPTENTRIES      1024    // ページテーブルあたりのPTEの数
+#define PGSIZE          4096    // 1ページによりマップされるバイト数
 
 #define PGSHIFT         12      // log2(PGSIZE)
-#define PTXSHIFT        12      // offset of PTX in a linear address
-#define PDXSHIFT        22      // offset of PDX in a linear address
+#define PTXSHIFT        12      // 線形アドレスにおけるPTXのオフセット
+#define PDXSHIFT        22      // 線形アドレスにおけるPDXのオフセット
 
 #define PGROUNDUP(sz)  (((sz)+PGSIZE-1) & ~(PGSIZE-1))
 #define PGROUNDDOWN(a) (((a)) & ~(PGSIZE-1))
 
-// Page table/directory entry flags.
-#define PTE_P           0x001   // Present
-#define PTE_W           0x002   // Writeable
-#define PTE_U           0x004   // User
-#define PTE_PWT         0x008   // Write-Through
-#define PTE_PCD         0x010   // Cache-Disable
-#define PTE_A           0x020   // Accessed
-#define PTE_D           0x040   // Dirty
-#define PTE_PS          0x080   // Page Size
-#define PTE_MBZ         0x180   // Bits must be zero
+// ページテーブル/ディレクトリエントリのフラグ (p.308)
+#define PTE_P           0x001   // 0: メモリ上に存在
+#define PTE_W           0x002   // 1: 書き込み可能
+#define PTE_U           0x004   // 2: ユーザ
+#define PTE_PWT         0x008   // 3: ライトスルー
+#define PTE_PCD         0x010   // 4: キャッシュ禁止
+#define PTE_A           0x020   // 5: アクセス
+#define PTE_D           0x040   // 6: ダーティ
+#define PTE_PS          0x080   // 7: ページサイズ
+#define PTE_MBZ         0x180   // 7-8: ビットは0固定
 
-// Address in page table or page directory entry
+// ページテーブル/ディレクトリエントリ内のアドレス
 #define PTE_ADDR(pte)   ((uint)(pte) & ~0xFFF)
 #define PTE_FLAGS(pte)  ((uint)(pte) &  0xFFF)
 
 #ifndef __ASSEMBLER__
 typedef uint pte_t;
 
-// Task state segment format
+// タスクステートセグメント形式
 struct taskstate {
-  uint link;         // Old ts selector
-  uint esp0;         // Stack pointers and segment selectors
-  ushort ss0;        //   after an increase in privilege level
+  uint link;         // 旧タスクステートセレクタ
+  uint esp0;         // 特権レベルが上がった後の
+  ushort ss0;        //   スタックポインタとセグメントセレクタ
   ushort padding1;
   uint *esp1;
   ushort ss1;
@@ -161,10 +161,10 @@ struct taskstate {
   uint *esp2;
   ushort ss2;
   ushort padding3;
-  void *cr3;         // Page directory base
-  uint *eip;         // Saved state from last task switch
+  void *cr3;         // ページディレクトリのベース
+  uint *eip;         // 直前のタスクスイッチで保存されたステート
   uint eflags;
-  uint eax;          // More saved state (registers)
+  uint eax;          // さらに保存されたステート（レジスタ）
   uint ecx;
   uint edx;
   uint ebx;
@@ -172,7 +172,7 @@ struct taskstate {
   uint *ebp;
   uint esi;
   uint edi;
-  ushort es;         // Even more saved state (segment selectors)
+  ushort es;         // さらにさらに保存されたステート（セグメントセレクタ）
   ushort padding4;
   ushort cs;
   ushort padding5;
@@ -186,32 +186,32 @@ struct taskstate {
   ushort padding9;
   ushort ldt;
   ushort padding10;
-  ushort t;          // Trap on task switch
-  ushort iomb;       // I/O map base address
+  ushort t;          // タスクスイッチを起こしたトラップ
+  ushort iomb;       // I/Oマップのベースアドレス
 };
 
 // PAGEBREAK: 12
-// Gate descriptors for interrupts and traps
+// 割り込みとトラップ用のゲートディスクリプタ
 struct gatedesc {
-  uint off_15_0 : 16;   // low 16 bits of offset in segment
-  uint cs : 16;         // code segment selector
-  uint args : 5;        // # args, 0 for interrupt/trap gates
-  uint rsv1 : 3;        // reserved(should be zero I guess)
-  uint type : 4;        // type(STS_{TG,IG32,TG32})
-  uint s : 1;           // must be 0 (system)
-  uint dpl : 2;         // descriptor(meaning new) privilege level
-  uint p : 1;           // Present
-  uint off_31_16 : 16;  // high bits of offset in segment
+  uint off_15_0 : 16;   // セグメントオフセットの低位16ビット
+  uint cs : 16;         // コードセグメントセレクタ
+  uint args : 5;        // 引数の数、割り込み/トラップゲートでは0
+  uint rsv1 : 3;        // 予約済（0にするべきだと思う）
+  uint type : 4;        // タイプ(STS_{TG,IG32,TG32})
+  uint s : 1;           // 0固定（システム）
+  uint dpl : 2;         // ディスクリプタ（の新しい）特権レベル
+  uint p : 1;           // 存在する
+  uint off_31_16 : 16;  // セグメントオフセットの高位16ビット
 };
 
-// Set up a normal interrupt/trap gate descriptor.
-// - istrap: 1 for a trap (= exception) gate, 0 for an interrupt gate.
-//   interrupt gate clears FL_IF, trap gate leaves FL_IF alone
-// - sel: Code segment selector for interrupt/trap handler
-// - off: Offset in code segment for interrupt/trap handler
-// - dpl: Descriptor Privilege Level -
-//        the privilege level required for software to invoke
-//        this interrupt/trap gate explicitly using an int instruction.
+// 通常の割り込み/ゲートディスクリプタを設定する。
+// - istrap: 1 はトラップ（=例外）ゲート、0 は割り込みゲート。
+//   割り込みゲートはFL_FLをクリアするが、トラップゲートはFL_IFをいじらない
+// - sel: 割り込み/トラップハンドラ用のコードセグメントセレクタ
+// - off: 割り込み/トラップハンドラ用のコードセグメント内のオフセット
+// - dpl: ディスクリプタ特権レベル -
+//        ソフトウェアがint命令を使ってこの割り込み/トラップゲートを
+//        明示的に実行するために必要な特権レベル
 #define SETGATE(gate, istrap, sel, off, d)                \
 {                                                         \
   (gate).off_15_0 = (uint)(off) & 0xffff;                \
