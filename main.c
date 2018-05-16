2065d8f1a9914009879cd1f69c9493db2fb77983
Keiji Suzuki <zuki.ebetsu@gmail.com>
2018-05-16 17:49:10 +0900

    translate the comments in main.c

--- main.c
+++ main.c
@@ -9,35 +9,35 @@
 static void startothers(void);
 static void mpmain(void)  __attribute__((noreturn));
 extern pde_t *kpgdir;
-extern char end[]; // first address after kernel loaded from ELF file
+extern char end[]; // カーネルをELFファイルからロードした後の最初のアドレス
 
-// Bootstrap processor starts running C code here.
-// Allocate a real stack and switch to it, first
-// doing some setup required for memory allocator to work.
+// ブートストラップププロセッサはここからCコードの実行を開始する。
+// 実際のスタックを割り当て、それに切り替える。まず、メモリアロケータの
+// 動作に必要な設定を行う
 int
 main(void)
 {
-  kinit1(end, P2V(4*1024*1024)); // phys page allocator
-  kvmalloc();      // kernel page table
-  mpinit();        // detect other processors
-  lapicinit();     // interrupt controller
-  seginit();       // segment descriptors
-  picinit();       // disable pic
-  ioapicinit();    // another interrupt controller
-  consoleinit();   // console hardware
-  uartinit();      // serial port
-  pinit();         // process table
-  tvinit();        // trap vectors
-  binit();         // buffer cache
-  fileinit();      // file table
-  ideinit();       // disk 
-  startothers();   // start other processors
-  kinit2(P2V(4*1024*1024), P2V(PHYSTOP)); // must come after startothers()
-  userinit();      // first user process
-  mpmain();        // finish this processor's setup
+  kinit1(end, P2V(4*1024*1024)); // 物理ページアロケータ
+  kvmalloc();      // カーネルページテーブル
+  mpinit();        // 他のプロセッサの検出
+  lapicinit();     // 割り込みコントローラ
+  seginit();       // セグメントデスクリプタ
+  picinit();       // picの無効化
+  ioapicinit();    // もう1つの割り込みコントローラ
+  consoleinit();   // コンソールハードウェア
+  uartinit();      // シリアルポート
+  pinit();         // プロセステーブル
+  tvinit();        // トラップベクタ
+  binit();         // バッファキャッシュ
+  fileinit();      // ファイルテーブル
+  ideinit();       // ディスク
+  startothers();   // 他のプロセッサの開始
+  kinit2(P2V(4*1024*1024), P2V(PHYSTOP)); // startothers()の後でなければならない
+  userinit();      // 最初のユーザプロセス
+  mpmain();        // このプロセッサの設定を終了
 }
 
-// Other CPUs jump here from entryother.S.
+// 他のCPUはentryother.Sからここにジャンプする
 static void
 mpenter(void)
 {
@@ -47,19 +47,19 @@ mpenter(void)
   mpmain();
 }
 
-// Common CPU setup code.
+// 共通のCPU設定コード
 static void
 mpmain(void)
 {
   cprintf("cpu%d: starting %d\n", cpuid(), cpuid());
-  idtinit();       // load idt register
-  xchg(&(mycpu()->started), 1); // tell startothers() we're up
-  scheduler();     // start running processes
+  idtinit();       // idtレジスタのロード
+  xchg(&(mycpu()->started), 1); // startothers()にこのCPUの起動を伝える
+  scheduler();     // プロセスの実行を開始する
 }
 
-pde_t entrypgdir[];  // For entry.S
+pde_t entrypgdir[];  // entry.Sのために
 
-// Start the non-boot (AP) processors.
+// 非ブート(AP)プロセッサを開始する
 static void
 startothers(void)
 {
@@ -68,19 +68,19 @@ startothers(void)
   struct cpu *c;
   char *stack;
 
-  // Write entry code to unused memory at 0x7000.
-  // The linker has placed the image of entryother.S in
-  // _binary_entryother_start.
+  // エントリコードを0x7000の未使用メモリに書き込む
+  // リンカはentryother.Sのイメージを_binary_entryother_startに
+  // 置いた。
   code = P2V(0x7000);
   memmove(code, _binary_entryother_start, (uint)_binary_entryother_size);
 
   for(c = cpus; c < cpus+ncpu; c++){
-    if(c == mycpu())  // We've started already.
+    if(c == mycpu())  // ブートCPUは開始済み
       continue;
 
-    // Tell entryother.S what stack to use, where to enter, and what
-    // pgdir to use. We cannot use kpgdir yet, because the AP processor
-    // is running in low  memory, so we use entrypgdir for the APs too.
+    // どのスタックを使うのか、どこにenterするのか、どのpgdirを使うのかを
+    // entryother.Sに伝える。APプロセッサは低位アドレスで実効しているので
+    // kpgdirはまだ使えない。そのため、APにもentrypgdirを使用する。
     stack = kalloc();
     *(void**)(code-4) = stack + KSTACKSIZE;
     *(void**)(code-8) = mpenter;
@@ -88,22 +88,22 @@ startothers(void)
 
     lapicstartap(c->apicid, V2P(code));
 
-    // wait for cpu to finish mpmain()
+    // cpuがmpmain()を終了するのを待機する
     while(c->started == 0)
       ;
   }
 }
 
-// The boot page table used in entry.S and entryother.S.
-// Page directories (and page tables) must start on page boundaries,
-// hence the __aligned__ attribute.
-// PTE_PS in a page directory entry enables 4Mbyte pages.
+// entry.S と entryother.Sで使われるブートページテーブル.
+// ページディレクトリ（とページエントリ）はページ境界から始めなければならない、
+// そのため __aligned__ attribute を指定する。
+// ページディレクトリエントリの PTE_PS はページサイズを4Mバイトにする。
 
 __attribute__((__aligned__(PGSIZE)))
 pde_t entrypgdir[NPDENTRIES] = {
-  // Map VA's [0, 4MB) to PA's [0, 4MB)
+  // 仮想アドレス [0, 4MB) を物理アドレス [0, 4MB) にマップ
   [0] = (0) | PTE_P | PTE_W | PTE_PS,
-  // Map VA's [KERNBASE, KERNBASE+4MB) to PA's [0, 4MB)
+  // 仮想アドレス [KERNBASE, KERNBASE+4MB) を物理アドレス [0, 4MB)
   [KERNBASE>>PDXSHIFT] = (0) | PTE_P | PTE_W | PTE_PS,
 };
 
@@ -113,4 +113,3 @@ pde_t entrypgdir[NPDENTRIES] = {
 // Blank page.
 //PAGEBREAK!
 // Blank page.
-
