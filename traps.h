c210885a1885c3ac7e2f881b68b5ca888df6eb82
Keiji Suzuki <zuki.ebetsu@gmail.com>
2018-05-17 11:59:59 +0900

    translate the comments in traps.h

--- traps.h
+++ traps.h
@@ -1,33 +1,33 @@
-// x86 trap and interrupt constants.
+// x86のトラップと割り込み関係の定数
 
-// Processor-defined:
-#define T_DIVIDE         0      // divide error
-#define T_DEBUG          1      // debug exception
-#define T_NMI            2      // non-maskable interrupt
-#define T_BRKPT          3      // breakpoint
-#define T_OFLOW          4      // overflow
-#define T_BOUND          5      // bounds check
-#define T_ILLOP          6      // illegal opcode
-#define T_DEVICE         7      // device not available
-#define T_DBLFLT         8      // double fault
-// #define T_COPROC      9      // reserved (not used since 486)
-#define T_TSS           10      // invalid task switch segment
-#define T_SEGNP         11      // segment not present
-#define T_STACK         12      // stack exception
-#define T_GPFLT         13      // general protection fault
-#define T_PGFLT         14      // page fault
-// #define T_RES        15      // reserved
-#define T_FPERR         16      // floating point error
-#define T_ALIGN         17      // aligment check
-#define T_MCHK          18      // machine check
-#define T_SIMDERR       19      // SIMD floating point error
+// プロセッサで定義:
+#define T_DIVIDE         0      // 除算エラー
+#define T_DEBUG          1      // デバッグ例外
+#define T_NMI            2      // NMI割り込み
+#define T_BRKPT          3      // ブレークポイント
+#define T_OFLOW          4      // オーバーフロー
+#define T_BOUND          5      // BOUND範囲超過
+#define T_ILLOP          6      // 無効オペコード
+#define T_DEVICE         7      // デバイス使用不可
+#define T_DBLFLT         8      // ダブルフォルト
+// #define T_COPROC      9      // 予約済（486以降未使用）
+#define T_TSS           10      // 無効TSS
+#define T_SEGNP         11      // セグメント不在
+#define T_STACK         12      // スタックフォルト例外
+#define T_GPFLT         13      // 一般保護例外
+#define T_PGFLT         14      // ページフォルト
+// #define T_RES        15      // 予約済
+#define T_FPERR         16      // 浮動小数点エラー
+#define T_ALIGN         17      // アライメントチェック
+#define T_MCHK          18      // マシンチェック
+#define T_SIMDERR       19      // SIMD浮動小数点エラー
 
-// These are arbitrarily chosen, but with care not to overlap
-// processor defined exceptions or interrupt vectors.
-#define T_SYSCALL       64      // system call
-#define T_DEFAULT      500      // catchall
+// 以下は任意に選択したものであるが、プロセッサが定義している
+// 例外や割り込みベクタと重ならないように注意はしている。
+#define T_SYSCALL       64      // システムコール
+#define T_DEFAULT      500      // キャッチオール
 
-#define T_IRQ0          32      // IRQ 0 corresponds to int T_IRQ
+#define T_IRQ0          32      // IRQ 0 はInt T_IRQに相当する
 
 #define IRQ_TIMER        0
 #define IRQ_KBD          1
@@ -35,4 +35,3 @@
 #define IRQ_IDE         14
 #define IRQ_ERROR       19
 #define IRQ_SPURIOUS    31
-
