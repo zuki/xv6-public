6e79db8a33a4b3fd46f592f5d483056f475332dd
Keiji Suzuki <zuki.ebetsu@gmail.com>
2018-05-20 08:49:54 +0900

    translate the comments in mp.h

--- mp.h
+++ mp.h
@@ -1,56 +1,56 @@
-// See MultiProcessor Specification Version 1.[14]
+// マルチプロセッサ仕様書第1巻を参照 [14]
 
-struct mp {             // floating pointer
+struct mp {             // 浮動ポインタ
   uchar signature[4];           // "_MP_"
-  void *physaddr;               // phys addr of MP config table
+  void *physaddr;               // MP構成テーブルの物理アドレス
   uchar length;                 // 1
   uchar specrev;                // [14]
-  uchar checksum;               // all bytes must add up to 0
-  uchar type;                   // MP system config type
+  uchar checksum;               // すべてのバイトの合計は0でなければならない
+  uchar type;                   // MPシステム構成種別
   uchar imcrp;
   uchar reserved[3];
 };
 
-struct mpconf {         // configuration table header
+struct mpconf {         // 構成テーブルヘッダ
   uchar signature[4];           // "PCMP"
-  ushort length;                // total table length
+  ushort length;                // 総テーブル長
   uchar version;                // [14]
-  uchar checksum;               // all bytes must add up to 0
-  uchar product[20];            // product id
-  uint *oemtable;               // OEM table pointer
-  ushort oemlength;             // OEM table length
-  ushort entry;                 // entry count
-  uint *lapicaddr;              // address of local APIC
-  ushort xlength;               // extended table length
-  uchar xchecksum;              // extended table checksum
+  uchar checksum;               // すべてのバイトの合計は0でなければならない
+  uchar product[20];            // 製品ID
+  uint *oemtable;               // OEM テーブルポインタ
+  ushort oemlength;             // OEM テーブル長
+  ushort entry;                 // エントリカウント
+  uint *lapicaddr;              // ローカルAPICのアドレス
+  ushort xlength;               // 拡張テーブル長
+  uchar xchecksum;              // 拡張テーブルチェックサム
   uchar reserved;
 };
 
-struct mpproc {         // processor table entry
-  uchar type;                   // entry type (0)
-  uchar apicid;                 // local APIC id
-  uchar version;                // local APIC verison
-  uchar flags;                  // CPU flags
-    #define MPBOOT 0x02           // This proc is the bootstrap processor.
-  uchar signature[4];           // CPU signature
-  uint feature;                 // feature flags from CPUID instruction
+struct mpproc {         // プロセッサテーブルエントリ
+  uchar type;                   // エントリ種別 (0)
+  uchar apicid;                 // ローカルAPIC ID
+  uchar version;                // ローカルAPIC バージョン
+  uchar flags;                  // CPU フラグ
+    #define MPBOOT 0x02           // このプロセッサはブートストラッププロセッサ
+  uchar signature[4];           // CPU シグネチャ
+  uint feature;                 // CPUID命令のfeatureフラグ
   uchar reserved[8];
 };
 
-struct mpioapic {       // I/O APIC table entry
-  uchar type;                   // entry type (2)
-  uchar apicno;                 // I/O APIC id
-  uchar version;                // I/O APIC version
-  uchar flags;                  // I/O APIC flags
-  uint *addr;                  // I/O APIC address
+struct mpioapic {       // I/O APIC テーブルエントリ
+  uchar type;                   // エントリ種別 (2)
+  uchar apicno;                 // I/O APIC ID
+  uchar version;                // I/O APIC バージョン
+  uchar flags;                  // I/O APIC フラグ
+  uint *addr;                  // I/O APIC アドレス
 };
 
-// Table entry types
-#define MPPROC    0x00  // One per processor
-#define MPBUS     0x01  // One per bus
-#define MPIOAPIC  0x02  // One per I/O APIC
-#define MPIOINTR  0x03  // One per bus interrupt source
-#define MPLINTR   0x04  // One per system interrupt source
+// テーブルエントリ種別
+#define MPPROC    0x00  // プロセッサごとに1つ
+#define MPBUS     0x01  // バスごとに1つ
+#define MPIOAPIC  0x02  // I/O APICごとに1つ
+#define MPIOINTR  0x03  // バス割り込みソースごとに1つ
+#define MPLINTR   0x04  // システム割り込みソースごとに1つ
 
 //PAGEBREAK!
 // Blank page.
