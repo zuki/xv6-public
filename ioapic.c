fad3f57155cca7aaaf93e6e8e28375502380184a
Keiji Suzuki <zuki.ebetsu@gmail.com>
2018-05-20 11:02:00 +0900

    translate the comments in ioapic.c

--- ioapic.c
+++ ioapic.c
@@ -1,30 +1,30 @@
-// The I/O APIC manages hardware interrupts for an SMP system.
+// I/O APICはSMPシステムのハードウェア割り込みを管理する。
 // http://www.intel.com/design/chipsets/datashts/29056601.pdf
-// See also picirq.c.
+// picirq.cも参照。
 
 #include "types.h"
 #include "defs.h"
 #include "traps.h"
 
-#define IOAPIC  0xFEC00000   // Default physical address of IO APIC
+#define IOAPIC  0xFEC00000   // IO APICのデフォルト物理アドレス
 
-#define REG_ID     0x00  // Register index: ID
-#define REG_VER    0x01  // Register index: version
-#define REG_TABLE  0x10  // Redirection table base
+#define REG_ID     0x00  // レジスタインデックス: ID
+#define REG_VER    0x01  // レジスタインデックス: バージョン
+#define REG_TABLE  0x10  // リダイレクションテーブルベース
 
-// The redirection table starts at REG_TABLE and uses
-// two registers to configure each interrupt.
-// The first (low) register in a pair contains configuration bits.
-// The second (high) register contains a bitmask telling which
-// CPUs can serve that interrupt.
-#define INT_DISABLED   0x00010000  // Interrupt disabled
-#define INT_LEVEL      0x00008000  // Level-triggered (vs edge-)
-#define INT_ACTIVELOW  0x00002000  // Active low (vs high)
-#define INT_LOGICAL    0x00000800  // Destination is CPU id (vs APIC ID)
+// リダイレクションテーブルはREG_TABLEから始まり、
+// 2つのレジスタを使って、各割り込みを構成する。
+// 最初の（低位）レジスタは構成ビットを持つ。
+// 2番めの（高位）レジスタは、どのCPUがその割り込みに対応できるかを
+// 示すビットマスクを持つ。
+#define INT_DISABLED   0x00010000  // 割り込みは禁止
+#define INT_LEVEL      0x00008000  // レベルトリガ（vs エッジトリガ）
+#define INT_ACTIVELOW  0x00002000  // アクティブロー（vs アクティブハイ）
+#define INT_LOGICAL    0x00000800  // 宛先はCPU id（vs APIC ID）
 
 volatile struct ioapic *ioapic;
 
-// IO APIC MMIO structure: write reg, then read or write data.
+// IO APIC MMIO 構造体: regを書き込み、次にdataの読み込み/書き込みを行う。
 struct ioapic {
   uint reg;
   uint pad[3];
@@ -56,8 +56,8 @@ ioapicinit(void)
   if(id != ioapicid)
     cprintf("ioapicinit: id isn't equal to ioapicid; not a MP\n");
 
-  // Mark all interrupts edge-triggered, active high, disabled,
-  // and not routed to any CPUs.
+  // すべての割り込みの設定を、エッジトリガ、アクティブハイ、禁止、
+  // どのCPUにも転送しない、とする。
   for(i = 0; i <= maxintr; i++){
     ioapicwrite(REG_TABLE+2*i, INT_DISABLED | (T_IRQ0 + i));
     ioapicwrite(REG_TABLE+2*i+1, 0);
@@ -67,9 +67,9 @@ ioapicinit(void)
 void
 ioapicenable(int irq, int cpunum)
 {
-  // Mark interrupt edge-triggered, active high,
-  // enabled, and routed to the given cpunum,
-  // which happens to be that cpu's APIC ID.
+  // 割り込みの設定を、エッジトリガ、アクティブハイ、受け付ける、
+  // 指定されたcpunumに転送する、とする。
+  // cpunumはそのcpuのAPIC IDであることもある。
   ioapicwrite(REG_TABLE+2*irq, T_IRQ0 + irq);
   ioapicwrite(REG_TABLE+2*irq+1, cpunum << 24);
 }
