d0012ffed84944c0a85112eb4dcd4c106765d34a
Keiji Suzuki <zuki.ebetsu@gmail.com>
2018-08-29 15:27:26 +0900

    divide user.h for using in newlib client

--- user.h
+++ user.h
@@ -1,32 +1,5 @@
-struct stat;
-struct rtcdate;
-
 // system calls
-int fork(void);
-int exit(void) __attribute__((noreturn));
-int wait(void);
-int pipe(int*);
-int write(int, void*, int);
-int read(int, void*, int);
-int close(int);
-int kill(int);
-int exec(char*, char**);
-int open(char*, int);
-int mknod(char*, short, short);
-int unlink(char*);
-int fstat(int fd, struct stat*);
-int link(char*, char*);
-int mkdir(char*);
-int chdir(char*);
-int dup(int);
-int getpid(void);
-char* sbrk(int);
-int sleep(int);
-int uptime(void);
-int date(struct rtcdate*);
-int dup2(int, int);
-int alarm(int, void(*)());
-int lseek(int, int, int);
+#include "user_syscall.h"
 
 // ulib.c
 int stat(char*, struct stat*);
