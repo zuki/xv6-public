7b26d372d33a97fc1bcbef89be6ab34b98659df1
Keiji Suzuki <zuki.ebetsu@gmail.com>
2018-09-04 17:38:13 +0900

    edit include files in cat.c

--- usr/cat.c
+++ usr/cat.c
@@ -1,9 +1,6 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
 #include <unistd.h>
 
 char buf[512];
